499cd04b7c435792a9120627231bac7054037915
Phean <phean.p07@gmail.com>
2020-03-21 23:37:35 +0700

    Add latest

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Model\Review;
 
 class Product extends Model
-{
+{ 
     public function Review()
     {
         return $this->hasMany(Review::class);
